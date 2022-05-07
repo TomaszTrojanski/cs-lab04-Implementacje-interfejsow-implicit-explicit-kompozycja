5042143670c6a0fb34e3b0d5e38085c31ad2e530
TomaszTrojanski <79982724+TomaszTrojanski@users.noreply.github.com>
2022-05-07 13:02:13 +0200

    tak

---
+++ Zadanie1/Copier.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie1
+{
+    public class Copier : IPrinter, IScanner
+    {
+        public int Counter { get; private set; } = 0;
+        public int ScanCounter { get; private set; } = 0;
+        public int PrintCounter { get; private set; } = 0;
+
+        protected IDevice.State state = IDevice.State.off;
+        public IDevice.State GetState() => state;
+
+        public void PowerOff()
+        {
+            state = IDevice.State.off;
+            Console.WriteLine("... Device is off !");
+        }
+
+        public void PowerOn()
+        {
+            if (state == IDevice.State.off)
+            {
+                state = IDevice.State.on;
+                Counter++;
+            }
+            Console.WriteLine("Device is on ...");
+        }
+
+        public void Print(in IDocument document)
+        {
+            if (state == IDevice.State.on)
+            {
+                PrintCounter++;
+                Console.WriteLine($"{DateTime.Now} Print: {document.GetFileName()}");
+            }
+
+        }
+
+        public void Scan(out IDocument document, IDocument.FormatType formatType = IDocument.FormatType.PDF)
+        {
+            if (formatType == IDocument.FormatType.PDF)
+            {
+                document = new PDFDocument("cos.pdf");
+            }
+            else if (formatType == IDocument.FormatType.JPG)
+            {
+                document = new ImageDocument("cos.jpg");
+            }
+            else
+            {
+                document = new TextDocument("cos.txt");
+            }
+
+            if (state == IDevice.State.on)
+            {
+                ScanCounter++;
+                Console.WriteLine($"{DateTime.Now} Scan: {document.GetFileName()}");
+            }
+        }
+
+        public void ScanAndPrint()
+        {
+            if (state == IDevice.State.on)
+            {
+                IDocument doc;
+                Scan(out doc, formatType: IDocument.FormatType.JPG);
+                Print(doc);
+            }
+
+        }
+    }
+}
---
+++ Zadanie3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Zadanie3
+{
+    class Program
+    {
+        static void Main()
+        {
+            var xeroxp = new Printer();
+            var xeroxs = new Scanner();           
+            var xerox = new Copier(xeroxp, xeroxs);          
+            
+            xerox.PowerOn();           
+            IDocument doc1 = new PDFDocument("cos.pdf");
+            xerox.Print(in doc1);
+            IDocument doc2;
+            
+            xerox.Scan(out doc2);
+            xerox.Scan(out doc2, formatType: IDocument.FormatType.JPG);
+            
+            xerox.ScanAndPrint();
+            System.Console.WriteLine(xerox.Counter);
+            System.Console.WriteLine(xeroxp.PrintCounter);
+            System.Console.WriteLine(xerox.ScanCounter);
+
+        }
+    }
+}
